c280a86270d2aa04fda9eced8667efb52d9897e6
Dimas <dimaskurniawan40@gmail.com>
2024-01-16 00:37:19 +0700

    fix: fix category on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -113,7 +113,7 @@
                                                 </td>
                                                 <td
                                                     class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                    {{ $book->category->category_name }}
+                                                    {{ $book->category->name }}
                                                 </td>
                                                 <td
                                                     class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
